212e034f6bc92fda102f2bbd0f79df14c281d37a
pythonDev161221 <adverbekreklamov@gmail.com>
2022-03-05 12:44:15 +0600

    add model Review

--- review_project/webapp/models.py
+++ review_project/webapp/models.py
@@ -1,11 +1,26 @@
+from django.contrib.auth import get_user_model
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
 # Create your models here.
 PRODUCT_CATEGORY = (("CLOTHES", "Одежда"), ("TECH", "Техника"), ("SERVICE", "Услуги"))
+
+User = get_user_model()
 
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
     category = models.CharField(max_length=50, choices=PRODUCT_CATEGORY, default="CLOTHES")
     description = models.CharField(max_length=2000, null=True, blank=True)
-    image = models.ImageField(upload_to="pictures/", default="static/images/images.png")
+    image = models.ImageField(upload_to="pictures/", default="static/images/images.png",
+                              null=True, blank=True)
+
+
+class Review(models.Model):
+    author = models.ForeignKey(User, related_name="reviews", on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, related_name='reviews', on_delete=models.CASCADE)
+    text = models.CharField(max_length=2000)
+    evaluation = models.PositiveIntegerField(validators=(MaxValueValidator(5,),))
+    is_moderated = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
